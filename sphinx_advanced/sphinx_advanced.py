55cdb882bb8961c58f490ca25cabb86a2d3ba8be
Julio Antúnez Tarín <julio.antunez.tarin@gmail.com>
2018-07-29 18:34:54 +0200

    feat(main): add a new help function to main module

--- sphinx_advanced/sphinx_advanced.py
+++ sphinx_advanced/sphinx_advanced.py
@@ -1,3 +1,29 @@
 # -*- coding: utf-8 -*-
 
-"""Main module."""
+"""Main module
+
+.. warning:: Proof of Concept
+
+.. module:: sphinx_advanced.sphinx_advanced
+    :platform: OS X
+    :synopsis: sphinx advanced tutorial project
+
+.. moduleauthor:: Julio Antúnez Tarín <julio.antunez.tarin@gmail.com>
+"""
+
+
+def help():
+    """Sphinx Advanced Module help
+
+    Args:
+        none
+
+    Retuns:
+        A brief description of the module
+
+    """
+    print('Sphinx Advanced Tutoroial Module')
+
+
+if __name__ == "__main__":
+    help()
